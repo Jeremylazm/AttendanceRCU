206f01a582abb33e9f4dfb9ab38f61450c46e6c7
Jeremy Axl Lazo Mendoza <182916@unsaac.edu.pe>
2023-03-17 11:35:29 -0500

    login screen

---
+++ rcu-assistant/rcu_assistant/lib/app_icons.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  static const String userIcon = 'assets/images/user_icon.svg';
+  static const String passwordIcon = 'assets/images/password_icon.svg';
+}
