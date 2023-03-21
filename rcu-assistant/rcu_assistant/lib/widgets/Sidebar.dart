f2a383e03764b7fec1ed6a79bb854846804f8d7c
Jose Angel <171573@unsaac.edu.pe>
2023-03-21 12:50:18 -0500

    Cambio de vista en el main

--- rcu-assistant/rcu_assistant/lib/widgets/Sidebar.dart
+++ rcu-assistant/rcu_assistant/lib/widgets/Sidebar.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:rcu_assistant/configs/app_colors.dart';
 import 'package:rcu_assistant/configs/app_styles.dart';
 
@@ -30,28 +29,45 @@ class Sidebar extends StatelessWidget {
           SizedBox(height: is_large_screen ? 20 : 5,),
           Image.asset("assets/images/logo_rcu_fondo_transparente.png", height: is_large_screen ? 80 : 60,),
           Expanded(child: Container(),),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-              margin: const EdgeInsets.all(15),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: AppColors.whiteColor, foregroundColor: AppColors.mainBlueColor, elevation: 0),
-                onPressed: (){}, 
-                child: SizedBox(
-                  width: 60,
-                  height: 40,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Icon(Icons.exit_to_app, size: 20,),
-                      is_large_screen ? Text("Salir", style: poppinsStyle.copyWith(fontWeight: FontWeight.bold),) : Container(),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          )
+          const ExitButton(),
         ],
+      ),
+    );
+  }
+}
+
+
+
+
+class ExitButton extends StatelessWidget {
+
+  const ExitButton({super.key,});
+    
+  @override
+  Widget build(BuildContext context) {
+
+    final width_screen = MediaQuery.of(context).size.width;
+    final is_large_screen = width_screen > 800;
+
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: Container(
+        margin: const EdgeInsets.all(15),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(backgroundColor: AppColors.whiteColor, foregroundColor: AppColors.mainBlueColor, elevation: 0),
+          onPressed: (){}, 
+          child: SizedBox(
+            width: 60,
+            height: 40,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(Icons.exit_to_app, size: 20,),
+                is_large_screen ? Text("Salir", style: poppinsStyle.copyWith(fontWeight: FontWeight.bold),) : Container(),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
