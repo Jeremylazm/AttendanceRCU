377871d8b588f8882f847effe22ff7d790bcda2a
Jeremy Axl Lazo Mendoza <182916@unsaac.edu.pe>
2023-03-29 12:44:31 -0500

    botones en tabla

--- rcu-attendance/rcu_attendance/lib/main.dart
+++ rcu-attendance/rcu_attendance/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'RCU Assistant',
+      title: 'RCU Attendance',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
