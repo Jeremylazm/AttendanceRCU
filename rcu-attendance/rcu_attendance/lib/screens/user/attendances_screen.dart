2984cbd52b8f278e6240c58bbef4b9be17ac9f08
Jeremy Axl Lazo Mendoza <182916@unsaac.edu.pe>
2023-03-30 10:35:15 -0500

    vista de ordenes de trabajo

--- rcu-attendance/rcu_attendance/lib/screens/user/attendances_screen.dart
+++ rcu-attendance/rcu_attendance/lib/screens/user/attendances_screen.dart
@@ -1,18 +1,20 @@
-
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:rcu_assistant/configs/app_colors.dart';
 import 'package:rcu_assistant/configs/app_styles.dart';
 import 'package:rcu_assistant/widgets/custom_title.dart';
 import 'package:rcu_assistant/widgets/widgets.dart';
 
 class AttendancesScreen extends StatelessWidget {
-const AttendancesScreen({ Key? key }) : super(key: key);
+  const AttendancesScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     final is_large_screen = MediaQuery.of(context).size.width > 800;
-
+    initializeDateFormatting();
+    DateTime now = DateTime.now();
+    var dateString = DateFormat('dd/MM/yyyy').format(now);
     return Expanded(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -20,8 +22,10 @@ const AttendancesScreen({ Key? key }) : super(key: key);
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              CustomTitle(text: "Asistencias"),
-              is_large_screen ? CustomTitle(text: "29/03/2023", fontSize: 18) : Container(),
+              const CustomTitle(text: "Asistencias"),
+              is_large_screen
+                  ? CustomTitle(text: dateString, fontSize: 18)
+                  : Container(),
             ],
           ),
           AttendancesTable(),
@@ -30,5 +34,3 @@ const AttendancesScreen({ Key? key }) : super(key: key);
     );
   }
 }
-
-
