59417f46cccf13d09f3d4db04670fef78b40e5ff
Jose Angel <171573@unsaac.edu.pe>
2023-04-10 09:19:53 -0500

    Creación de modelo WorkOrder y consulta a base de datos

---
+++ rcu-attendance/rcu_attendance/lib/providers/work_order_provider.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'package:mysql_client/mysql_client.dart';
+import 'package:rcu_assistant/models/work_order.dart';
+import 'package:rcu_assistant/services/services.dart';
+
+class WorkOrderProvider {
+
+  Future<List<WorkOrder>> getTodayOrders() async {
+    final List<WorkOrder> orders = [];
+    IResultSet ordersResult = await WorkOrderService.getWorkOrders();
+    for(var orderRow in ordersResult.rows){
+      WorkOrder? order = WorkOrder.fromIResult(orderRow);
+      if(order != null) orders.add(order);
+    }
+    return orders;
+  }
+
+}
