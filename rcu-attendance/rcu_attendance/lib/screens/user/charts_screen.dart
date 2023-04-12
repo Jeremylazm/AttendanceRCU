53a342519fc0f9da316009f16f6f2461dd4f339f
UNSAAC <182916@unsaac.edu.pe>
2023-04-12 12:52:04 -0500

    charts

--- rcu-attendance/rcu_attendance/lib/screens/user/charts_screen.dart
+++ rcu-attendance/rcu_attendance/lib/screens/user/charts_screen.dart
@@ -1,56 +1,117 @@
-import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:rcu_assistant/tools/model/sample_view.dart';
+import 'dart:math';
 
-class ChartsScreen extends StatelessWidget {
-  ChartsScreen({ Key? key }) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:rcu_assistant/configs/app_colors.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+
+class ChartsScreen extends StatefulWidget {
+  // ignore: prefer_const_constructors_in_immutables
+  ChartsScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChartsScreenState createState() => _ChartsScreenState();
+}
+
+class _ChartsScreenState extends State<ChartsScreen> {
+
+  int count = 11;
+  List<_SalesData> data = [
+    _SalesData('Jan', 35),
+    _SalesData('Feb', 28),
+    _SalesData('Mar', 34),
+    _SalesData('Apr', 32),
+    _SalesData('May', 40)
+  ];
+
+  // Initialized a global variable for ChartSeriesController class
+  ChartSeriesController? _chartSeriesController;
 
   @override
   Widget build(BuildContext context) {
-    return SfCartesianChart(
-      plotAreaBorderWidth: 0,
-      title: ChartTitle(
-          text: isCardView ? '' : 'Population growth of various countries'),
-      primaryXAxis: CategoryAxis( 
-        majorGridLines: const MajorGridLines(width: 0),
-      ),
-      primaryYAxis: NumericAxis(
-          axisLine: const AxisLine(width: 0),
-          labelFormat: '{value}%',
-          majorTickLines: const MajorTickLines(size: 0)),
-      series: _getDefaultColumnSeries(),
-      tooltipBehavior: _tooltipBehavior,
-    );
+    return Column(
+          children: [
+          //Initialize the chart widget
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+              height: 550,
+              width: 550,
+              child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              // Chart title
+              title: ChartTitle(text: 'Half yearly sales analysis'),
+              // Enable legend
+              legend: Legend(isVisible: true),
+              // Enable tooltip
+              tooltipBehavior: TooltipBehavior(enable: true),
+              series: <ChartSeries<_SalesData, String>>[
+                LineSeries<_SalesData, String>(
+                    dataSource: data,
+                    xValueMapper: (_SalesData sales, _) => sales.year,
+                    yValueMapper: (_SalesData sales, _) => sales.sales,
+                    name: 'Sales',
+                    // Enable data label
+                    dataLabelSettings: DataLabelSettings(isVisible: true))
+              ]),
+              ),
+              Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              //Initialize the spark charts widget
+              child: SfSparkLineChart.custom(
+                //Enable the trackball
+                trackball: SparkChartTrackball(
+                    activationMode: SparkChartActivationMode.tap),
+                //Enable marker
+                marker: SparkChartMarker(
+                    displayMode: SparkChartMarkerDisplayMode.all),
+                //Enable data label
+                labelDisplayMode: SparkChartLabelDisplayMode.all,
+                xValueMapper: (int index) => data[index].year,
+                yValueMapper: (int index) => data[index].sales,
+                dataCount: 5,
+              ),
+            ),
+          )
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //   children: [
+          //     Container(
+          //         child: ButtonTheme(
+          //             minWidth: 40.0,
+          //             height: 30.0,
+          //             child: ElevatedButton(
+          //               onPressed: () {
+          //                 _chartSeriesController?.animate();
+          //               },
+          //               child: Text('Animate line series',
+          //                   textScaleFactor: 1,
+          //                   style: TextStyle(color: Colors.white)),
+          //             ))),
+          //     Container(
+          //         child: ButtonTheme(
+          //             minWidth: 40.0,
+          //             height: 30.0,
+          //             child: ElevatedButton(
+          //               onPressed: () {
+          //                 addChartData();
+          //               },
+          //               child: Text('Add data point',
+          //                   textScaleFactor: 1,
+          //                   style: TextStyle(color: Colors.white)),
+          //             ))),
+          //   ],
+          // )
+        ]);
   }
+
 }
 
- List<ColumnSeries<ChartSampleData, String>> _getDefaultColumnSeries() {
-    return <ColumnSeries<ChartSampleData, String>>[
-      ColumnSeries<ChartSampleData, String>(
-        dataSource: <ChartSampleData>[
-          ChartSampleData(x: 'China', y: 0.541),
-          ChartSampleData(x: 'Brazil', y: 0.818),
-          ChartSampleData(x: 'Bolivia', y: 1.51),
-          ChartSampleData(x: 'Mexico', y: 1.302),
-          ChartSampleData(x: 'Egypt', y: 2.017),
-          ChartSampleData(x: 'Mongolia', y: 1.683),
-        ],
-        xValueMapper: (ChartSampleData sales, _) => sales.x as String,
-        yValueMapper: (ChartSampleData sales, _) => sales.y,
-        dataLabelSettings: const DataLabelSettings(
-            isVisible: true, textStyle: TextStyle(fontSize: 10)),
-      )
-    ];
-  }
+class _SalesData {
+  _SalesData(this.year, this.sales);
 
-class SalesData {
-  SalesData(this.year, this.sales);
-  final DateTime year;
+  final String year;
   final double sales;
-}
-class _ChartData {
-  _ChartData(this.x, this.y);
-
-  final String x;
-  final double y;
 }
